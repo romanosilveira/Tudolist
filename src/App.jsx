4cc9d96387b5d06ca55cd3f7e4baf00b416c732a
alexanderrjstj <alexanderpaolo.rojas@tajamar365.com>
2025-05-06 20:00:05 +0200

    base app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './assets/components/Header'
+import Form from './assets/components/Form'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Form />
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='bg-purple-500 flex justify-between p-4 shadow-md shadow-purple-400'>
+            <a href="https://github.com/romanosilveira/Tudolist.git">
+
+            </a>
+            <h1 className='text-white text-4xl font-bold'>To do list</h1>
+            <div>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
